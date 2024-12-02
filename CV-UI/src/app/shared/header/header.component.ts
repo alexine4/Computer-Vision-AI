79e1b06bf94fed8ba115ba5289e0ddf8b27c1e06
alexine4 <88242759+alexine4@users.noreply.github.com>
2024-12-02 22:10:11 +0200

    add header with styles (without resizing)

---
+++ CV-UI/src/app/shared/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatButtonModule, 
+    MatMenuModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
