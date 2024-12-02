0d21e47419f959a68568e878fdfe7983033bd531
alexine4 <88242759+alexine4@users.noreply.github.com>
2024-12-02 12:38:41 +0200

    create UI app and start settings

---
+++ CV-UI/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+export const routes: Routes = [
+    {
+        path: '', redirectTo: '/welcome', pathMatch: 'full'
+    },
+    {
+        path: 'welcome', component: WelcomeComponent
+    }
+
+];
