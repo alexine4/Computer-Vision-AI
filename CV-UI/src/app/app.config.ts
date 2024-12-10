6d4e24d80eb07ec1430e3829ec9823114cbb8a26
alexine4 <88242759+alexine4@users.noreply.github.com>
2024-12-10 15:10:39 +0200

    add function translation and save active lang in cookies

--- CV-UI/src/app/app.config.ts
+++ CV-UI/src/app/app.config.ts
@@ -1,10 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient
+) => new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom([TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })])
+  ],
 };
--- CV-UI/src/app/shared/header/header.component.ts
+++ CV-UI/src/app/shared/header/header.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    MatButtonModule, 
-    MatMenuModule
-  ],
+  imports: [MatButtonModule, MatMenuModule, TranslateModule],
+  providers: [CookieService],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currentLanguage: string = 'en';
 
+  constructor(
+    private translate: TranslateService,
+    private cookieService: CookieService
+  ) {
+    this.translate.addLangs(['uk', 'en']);
+    //this.translate.setDefaultLang('en');
+    this.currentLanguage = this.cookieService.get('language') || 'en';
+    this.translate.use(this.currentLanguage);
+  }
+
+  useLanguage(language: string): void {
+    this.currentLanguage = language
+    this.cookieService.set('language', language, { expires: 7, path: '/' });
+    this.translate.use(language);
+  }
 }
